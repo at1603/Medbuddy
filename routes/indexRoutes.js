6a2cfa987dd3b10e60fe19636ee5c6b040314516
Harsh Pandey <harshpandey011@gmail.com>
2021-01-25 11:43:02 +0530

    modified mail.js path

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,45 +1,48 @@
 var express = require("express");
 var router = express.Router();
 
-var sendMail = require('./mail');
+var sendMail = require("../mail");
 //home page route
-router.get('/', function(req, res){
-   res.render("landing"); 
+router.get("/", function (req, res) {
+  res.render("landing");
 });
 
 //about page route
-router.get("/about", function(req, res){
-   res.render("about"); 
+router.get("/about", function (req, res) {
+  res.render("about");
 });
 
 //contact page route
-router.get("/contact", function(req, res){
-   res.render("contact");
+router.get("/contact", function (req, res) {
+  res.render("contact");
 });
 
-router.post('/email', (req, res) => {
-   const Data={
-    name :req.body.name,
-   email:req.body.email,
-   subject:req.body.subject,
-   text:req.body.text
-};
+router.post("/email", (req, res) => {
+  const Data = {
+    name: req.body.name,
+    email: req.body.email,
+    subject: req.body.subject,
+    text: req.body.text,
+  };
 
-   sendMail(Data.name, Data.email, Data.subject, Data.text, function(err, data) {
-       if (err) {
-           res.status(500).json({ message: 'Internal Error' });
-       } else {
-           res.redirect("/contact_us")
-           res.status({ message: 'Email sent!!!' });
-       }
-   });
-   
+  sendMail(
+    Data.name,
+    Data.email,
+    Data.subject,
+    Data.text,
+    function (err, data) {
+      if (err) {
+        res.status(500).json({ message: "Internal Error" });
+      } else {
+        res.redirect("/contact_us");
+        res.status({ message: "Email sent!!!" });
+      }
+    }
+  );
 });
 
-
-router.get("/timeline", function(req,res){
-   res.render("timeline");
+router.get("/timeline", function (req, res) {
+  res.render("timeline");
 });
-
 
 module.exports = router;
