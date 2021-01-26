c8a4c64be57f1dfb4c67d7926aab4453c8727b11
Harsh Pandey <harshpandey011@gmail.com>
2021-01-26 11:02:51 +0530

    replace the previous path with new path in routes

--- routes/hospHospRoutes.js
+++ routes/hospHospRoutes.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/consultDoc", function (req, res) {
-  res.render("section3/index");
+  res.render("hospHospSection/index");
 });
 
 module.exports = router;
