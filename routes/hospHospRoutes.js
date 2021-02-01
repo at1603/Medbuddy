a6584a0f0b2451fb490cd21afc9b2a3b16dbfc4b
Anmol Sahu <anmolsahu2k@gmail.com>
2021-02-01 09:01:55 +0530

    Add hospHospSection/index.ejs

--- routes/hospHospRoutes.js
+++ routes/hospHospRoutes.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/consultDoc", function (req, res) {
-  res.render("hospHospSection/index");
+router.get("/hospHospSection/initialPage", function(req, res) {
+    res.render("hospHospSection/index");
+});
+
+router.get("/hospHospSection/bloodBanks", function(req, res) {
+    res.render("hospHospSection/Bloodbanks/index")
+});
+
+router.get("/hospHospSection/organVault", function(req, res) {
+    res.render("hospHospSection/Organvault/index")
+});
+
+router.get("/hospHospSection/ambulance", function(req, res) {
+    res.render("hospHospSection/Ambulance/index")
 });
 
 module.exports = router;
