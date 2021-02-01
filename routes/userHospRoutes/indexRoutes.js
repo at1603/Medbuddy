fe6cfe08cb9dde26793fa37dc6f9007229879c8a
Pro-Abhinav <abhinav16032002@gmail.com>
2021-02-01 23:44:24 +0530

    modified files

---
+++ routes/userHospRoutes/indexRoutes.js
@@ -0,0 +1,20 @@
+let express = require("express");
+let router = express.Router();
+
+router.get("/userHospSection/initialPage", function (req, res) {
+  res.render("userHospSection/index");
+});
+
+router.get("/userHospSection/bloodBanks", function(req, res){
+  res.render("userHospSection/BloodBanks/index", {flag: 0})
+});
+
+router.get("/userHospSection/organDonation", function(req, res){
+  res.render("userHospSection/Organdonation/index")
+});
+
+router.get("/userHospSection/searchHospitals", function(req, res){
+  res.render("userHospSection/searchHospitals/index")
+});
+
+module.exports = router;
