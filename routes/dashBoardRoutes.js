830642e4fcaf06a0876c7bdb1c315f2705ddc7c3
Pro-Abhinav <abhinav16032002@gmail.com>
2021-02-05 23:39:08 +0530

    fixed minor bugs

---
+++ routes/dashBoardRoutes.js
@@ -0,0 +1,20 @@
+let express = require('express');
+
+var router = express.Router();
+
+//----------Patient Routes--------------//
+//----X-----Patient Routes-------X------//
+
+//----------Doctor Routes--------------//
+//----X-----Doctor Routes--------x-----//
+
+//----------Hospital Admin Routes--------------//
+
+// router.get("/hospHospSection/initialPage", function(req, res) {
+//     res.render("hospHospSection/index");
+// });
+
+//-----X----Hospital Admin Routes-------X------//
+
+
+module.exports = router;
