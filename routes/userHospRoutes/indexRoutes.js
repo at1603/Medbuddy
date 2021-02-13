186b96c0f0b773fdf1a739d4191b5d10fa48c89f
Pro-Abhinav <abhinav16032002@gmail.com>
2021-02-13 22:20:06 +0530

    added flag variable

--- routes/userHospRoutes/indexRoutes.js
+++ routes/userHospRoutes/indexRoutes.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let router = express.Router();
+middleware = require("../../middlewares/authMiddlewares")
 
 router.get("/userHospSection/initialPage", function (req, res) {
   res.render("userHospSection/index");
@@ -9,8 +10,8 @@ router.get("/userHospSection/bloodBanks", function(req, res){
   res.render("userHospSection/BloodBanks/index", {flag: 0})
 });
 
-router.get("/userHospSection/organDonation", function(req, res){
-  res.render("userHospSection/Organdonation/index")
+router.get("/userHospSection/organDonation", middleware.isLoggedIn, function(req, res){
+  res.render("userHospSection/Organdonation/index" ,{flag: 0});
 });
 
 router.get("/userHospSection/searchHospitals", function(req, res){
