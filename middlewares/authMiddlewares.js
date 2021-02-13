127a4b9a6a7696c03a80e904b6229cc85567368c
Pro-Abhinav <abhinav16032002@gmail.com>
2021-02-13 22:18:55 +0530

    added middlewares

---
+++ middlewares/authMiddlewares.js
@@ -0,0 +1,12 @@
+const middlewareObj = {};
+
+middlewareObj.isLoggedIn = function(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    // req.flash("error", "Please Login First");
+    res.redirect("/userLogin");
+}
+
+
+module.exports = middlewareObj;
