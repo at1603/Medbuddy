c19b504f30c48b805bfa8fcb14aa889cbb794fe6
Pro-Abhinav <abhinav16032002@gmail.com>
2021-02-14 19:38:50 +0530

    added middlewares and bloodbank search

--- routes/userHospRoutes/indexRoutes.js
+++ routes/userHospRoutes/indexRoutes.js
@@ -2,11 +2,11 @@ let express = require("express");
 let router = express.Router();
 middleware = require("../../middlewares/authMiddlewares")
 
-router.get("/userHospSection/initialPage", function (req, res) {
+router.get("/userHospSection/initialPage", middleware.isLoggedIn, function (req, res) {
   res.render("userHospSection/index");
 });
 
-router.get("/userHospSection/bloodBanks", function(req, res){
+router.get("/userHospSection/bloodBanks", middleware.isLoggedIn, function(req, res){
   res.render("userHospSection/BloodBanks/index", {flag: 0})
 });
 
@@ -14,7 +14,7 @@ router.get("/userHospSection/organDonation", middleware.isLoggedIn, function(req
   res.render("userHospSection/Organdonation/index" ,{flag: 0});
 });
 
-router.get("/userHospSection/searchHospitals", function(req, res){
+router.get("/userHospSection/searchHospitals", middleware.isLoggedIn, function(req, res){
   res.render("userHospSection/searchHospitals/index")
 });
 
