7a5548a70554c573a9e05c2db2af8c4730fd5612
Pro-Abhinav <abhinav16032002@gmail.com>
2021-05-10 20:36:46 +0530

    [MODIFY] restructured schema

--- models/appointmentSchema.js
+++ models/appointmentSchema.js
@@ -3,10 +3,17 @@ var mongoose = require("mongoose");
 var appointmentSchema = new mongoose.Schema({
 
   roomId: { type: String, default: null },
-  relation: {
-    docId: { type: String, default: null },
-    patientId: { type: String, default: null },
+  patientId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Patient"
   },
+  docId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Doctor"
+  },
+  phone: {type: Number, required: true},
+  slot: {type: Date, required: false, default: null},
+  disease: {type: String, required: true}
 });
 
 module.exports = mongoose.model("Appointment", appointmentSchema);
